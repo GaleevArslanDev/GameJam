84ec6ac1aa66db87de694ae14ad53b45c6a12e9b
GaleevArslanDev <galeev_arslan2021@bk.ru>
2026-05-27 19:18:39 +0300

    Fix lightning, add first mini-game

---
+++ Assets/Scripts/Minigames/Core/MinigameController.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using Player;
+using Minigames.Throwing;
+using Unity.Cinemachine;
+
+namespace Minigames.Core
+{
+    public class MinigameController : MonoBehaviour
+    {
+        public static MinigameController Instance;
+
+        [Header("Player")]
+        [SerializeField] private GameObject player;
+
+        [SerializeField] private PlayerMotor playerMotor;
+        [SerializeField] private PlayerLook playerLook;
+
+        [SerializeField] private CharacterController characterController;
+
+        [Header("Cameras")]
+        [SerializeField] private CinemachineCamera fpsCamera;
+        [SerializeField] private CinemachineCamera minigameCamera;
+
+        [Header("Minigame")]
+        [SerializeField] private SideMoveController sideMoveController;
+        [SerializeField] private ProjectileThrower projectileThrower;
+
+        private Vector3 savedPosition;
+        private Quaternion savedRotation;
+
+        private ThrowingGame currentGame;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public void StartThrowingGame(
+            ThrowingGame game,
+            Transform playerPoint
+        )
+        {
+            currentGame = game;
+
+            SavePlayerState();
+
+            DisableFPSController();
+
+            TeleportPlayer(playerPoint);
+
+            EnableMinigameSystems();
+
+            SwitchToMinigameCamera();
+
+            currentGame.BeginGame();
+        }
+
+        public void FinishCurrentGame(bool success)
+        {
+            Debug.Log(
+                success
+                ? "MINIGAME WIN"
+                : "MINIGAME LOSE"
+            );
+
+            DisableMinigameSystems();
+
+            RestorePlayerState();
+
+            SwitchToFPSCamera();
+
+            EnableFPSController();
+
+            currentGame = null;
+        }
+
+        private void SavePlayerState()
+        {
+            savedPosition = player.transform.position;
+            savedRotation = player.transform.rotation;
+        }
+
+        private void RestorePlayerState()
+        {
+            characterController.enabled = false;
+
+            player.transform.position = savedPosition;
+            player.transform.rotation = savedRotation;
+
+            characterController.enabled = true;
+        }
+
+        private void TeleportPlayer(Transform point)
+        {
+            characterController.enabled = false;
+
+            player.transform.position = point.position;
+            player.transform.rotation = point.rotation;
+
+            characterController.enabled = true;
+        }
+
+        private void DisableFPSController()
+        {
+            playerMotor.enabled = false;
+            playerLook.enabled = false;
+        }
+
+        private void EnableFPSController()
+        {
+            playerMotor.enabled = true;
+            playerLook.enabled = true;
+        }
+
+        private void EnableMinigameSystems()
+        {
+            sideMoveController.EnableControl();
+            projectileThrower.EnableThrowing();
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        private void DisableMinigameSystems()
+        {
+            sideMoveController.DisableControl();
+            projectileThrower.DisableThrowing();
+        }
+
+        private void SwitchToMinigameCamera()
+        {
+            fpsCamera.Priority = 0;
+            minigameCamera.Priority = 20;
+        }
+
+        private void SwitchToFPSCamera()
+        {
+            fpsCamera.Priority = 20;
+            minigameCamera.Priority = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Minigames/Throwing/ProjectileThrower.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Minigames.Throwing
+{
+    public class ProjectileThrower : MonoBehaviour
+    {
+        [SerializeField] private GameObject projectilePrefab;
+
+        [SerializeField] private Transform shootPoint;
+
+        [SerializeField] private float throwForce = 20f;
+
+        [SerializeField] private int maxAmmo = 10;
+
+        private int currentAmmo;
+
+        private bool canThrow;
+
+        private void Awake()
+        {
+            currentAmmo = maxAmmo;
+        }
+
+        public void EnableThrowing()
+        {
+            canThrow = true;
+            currentAmmo = maxAmmo;
+        }
+
+        public void DisableThrowing()
+        {
+            canThrow = false;
+        }
+
+        private void Update()
+        {
+            if (!canThrow)
+                return;
+
+            if (
+                Mouse.current.leftButton
+                .wasPressedThisFrame
+            )
+            {
+                Throw();
+            }
+        }
+
+        private void Throw()
+        {
+            if (currentAmmo <= 0)
+            {
+                FindObjectOfType<ThrowingGame>()
+                    .LoseGame();
+
+                return;
+            }
+
+            currentAmmo--;
+
+            GameObject projectile =
+                Instantiate(
+                    projectilePrefab,
+                    shootPoint.position,
+                    shootPoint.rotation
+                );
+
+            Rigidbody rb =
+                projectile.GetComponent<Rigidbody>();
+
+            rb.AddForce(
+                shootPoint.forward * throwForce,
+                ForceMode.Impulse
+            );
+        }
+    }
+}
